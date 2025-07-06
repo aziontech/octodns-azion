35d17347a20e9f53fc347873d96de5355634d33f
Marcus Grando <marcus.grando@azion.com>
2025-07-06 09:06:30 -0300

    fix: added requests as dependency

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         ),
         'test': tests_require,
     },
-    install_requires=('octodns>=1.12.0',),
+    install_requires=('octodns>=1.12.0', 'requests>=2.32.4'),
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
